db1231ad4ab1f23aabc0a738fd7429f8e3a5730c
Ravil Gabdullin <gvg_ravil@mail.ru>
2021-06-30 13:48:05 +0300

    feat(mw): register user types and authorize

---
+++ internal/api/http/middleware/auth.go
@@ -0,0 +1,70 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	api "github.com/gxravel/bus-routes/internal/api/http"
+	"github.com/gxravel/bus-routes/internal/busroutes"
+	"github.com/gxravel/bus-routes/internal/busroutescontext"
+	"github.com/gxravel/bus-routes/internal/model"
+)
+
+func RegisterUserTypes(types ...model.UserType) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+			ctx = context.WithValue(ctx, busroutescontext.UserTypesKey, model.UserTypes(types))
+			r = r.WithContext(ctx)
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func Auth(busRoutes *busroutes.BusRoutes) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+
+			allowedUserTypes := busroutescontext.GetUserTypes(ctx)
+			token := getAuthToken(r)
+
+			user, err := busRoutes.UserByToken(ctx, token, allowedUserTypes...)
+			if err != nil {
+				switch err {
+				case busroutes.ErrPermissionDenied:
+					api.RespondError(ctx, w, http.StatusForbidden, err)
+					return
+				case busroutes.ErrTokenExpired:
+					api.RespondError(ctx, w, http.StatusUnauthorized, err)
+					return
+				default:
+					api.RespondError(ctx, w, http.StatusBadRequest, err)
+					return
+				}
+			}
+
+			ctx = context.WithValue(ctx, busroutescontext.UserKey, user)
+			ctx = context.WithValue(ctx, busroutescontext.TokenKey, token)
+
+			r = r.WithContext(ctx)
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+const (
+	AuthHeader = "Authorization"
+)
+
+func getAuthToken(r *http.Request) string {
+	tokens, ok := r.Header[AuthHeader]
+	if ok {
+		if len(tokens) == 1 {
+			return strings.TrimPrefix(tokens[0], "Bearer ")
+		}
+	}
+
+	return ""
+}
