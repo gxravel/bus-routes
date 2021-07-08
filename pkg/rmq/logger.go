94e21018ec343d04b71a1122ef027e2ff944b761
Ravil Gabdullin <gvg_ravil@mail.ru>
2021-07-08 16:37:28 +0300

    feat(rmq): add rabbitmq initial client implementation

---
+++ pkg/rmq/logger.go
@@ -0,0 +1,21 @@
+package rabbitmq
+
+type Logger interface {
+	Debugf(format string, data ...interface{})
+	Infof(format string, data ...interface{})
+	Warnf(format string, data ...interface{})
+	Errorf(format string, data ...interface{})
+	Fatalf(format string, data ...interface{})
+
+	Debug(msg string)
+	Info(msg string)
+	Warn(msg string)
+	Error(msg string)
+	Fatal(msg string)
+
+	WithField(name string, value interface{}) Logger
+	WithFields(pairs ...interface{}) Logger
+
+	WithErr(err error) Logger
+	WithStr(key string, val string) Logger
+}
